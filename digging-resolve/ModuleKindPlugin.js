3bc70dc57406b75a3a8505a7536dcd87e7ae522e
hucheng <hucheng@qutoutiao.net>
2019-09-05 14:31:00 +0800

    feat: 添加 依赖处理

--- digging-resolve/ModuleKindPlugin.js
+++ digging-resolve/ModuleKindPlugin.js
@@ -6,8 +6,9 @@
 
 
 module.exports = class ModuleKindPlugin {
-	constructor(source, target) {
+	constructor(source, modules, target) {
 		this.source = source;
+		this.modules = modules;
 		this.target = target;
 	}
 
@@ -15,13 +16,10 @@ module.exports = class ModuleKindPlugin {
 		const target = resolver.ensureHook(this.target);
 		resolver.getHook(this.source).tapAsync("ModuleKindPlugin", (request, callback) => {
 			if (!request.module) {
-				const obj = Object.assign({}, request, {
-					path: resolver.join(request.path, request.request),
-					relativePath: request.relativePath && resolver.join(request.relativePath, request.request),
-					request: undefined
-				});
-				return callback(null, obj);
+				return callback(null, request);
 			}
+			let array = request.path.split("/")
+			request.path = array.splice(array.length - 1, 0, this.modules)
 			const obj = Object.assign({}, request);
 
 			resolver.doResolve(target, obj, (err, result) => {
