78b880c6dd4f0e9b30abf2187130342d5075cd96
hucheng <hucheng@qutoutiao.net>
2019-09-06 13:13:30 +0800

    feat: 处理 node_module依赖问题

--- digging-resolve/DescriptionFilePlugin.js
+++ digging-resolve/DescriptionFilePlugin.js
@@ -3,25 +3,18 @@
  * @Date: 2019-09-05 13:15:04
  * @Description: here is des
  */
-/*
-MIT License http://www.opensource.org/licenses/mit-license.php
-Author Tobias Koppers @sokra
-*/
-"use strict";
 
 const DescriptionFileUtils = require("./DescriptionFileUtils");
 
 module.exports = class DescriptionFilePlugin {
-	constructor(source, filenames, target) {
+	constructor(source, filenames) {
 		this.source = source;
 		this.filenames = [].concat(filenames);
-		this.target = target;
 	}
 
 	apply(resolver) {
-		const target = resolver.ensureHook(this.target);
 		resolver.getHook(this.source).tapAsync("DescriptionFilePlugin", (request, callback) => {
-			const directory = request.path + "/node_modules";
+			const directory = request.path;
 			DescriptionFileUtils.loadDescriptionFile(resolver, directory, this.filenames, {}, (err, result) => {
 				if (err) return callback(err);
 				const relativePath = "." + request.path.substr(result.directory.length).replace(/\\/g, "/");
@@ -31,13 +24,8 @@ module.exports = class DescriptionFilePlugin {
 					descriptionFileRoot: result.directory,
 					relativePath: relativePath
 				});
-				resolver.doResolve(target, obj, "using description file: " + result.path + " (relative path: " + relativePath + ")", (err, result) => {
-					if (err) return callback(err);
-
-					// Don't allow other processing
-					if (result === undefined) return callback(null, null);
-					callback(null, result);
-				});
+				obj.path = obj.path + "/" + obj.descriptionFileData.main;
+				return callback(null, obj)
 			});
 		});
 	}
